61e549c612600ebd7490fd245556b2a8281fa386
gienini2 <gienini2@gmail.com>
2026-03-05 17:42:39 +0100

    Update users.js

--- users.js
+++ users.js
@@ -2,13 +2,13 @@
 // Format: { username, displayName }
 
 export const AUTHORIZED_USERS = new Map([
-  ['beta-1009-a', { displayName: 'Agent Beta-1009-A' }],
+  ['beta-1009-a', { displayName: 'Inspector Albert Jordan' }],
   ['beta-1009-b', { displayName: 'Agent Beta-1009-B' }],
-  ['beta-1009-c', { displayName: 'Agent Beta-1009-C' }],
+  ['beta-1009-c', { displayName: 'Caporal Daniel Chacon' }],
   ['beta-1009-d', { displayName: 'Agent Beta-1009-D' }],
-  ['beta-1009-e', { displayName: 'Agent Beta-1009-E' }],
-  ['beta-1009-f', { displayName: 'Agent Beta-1009-F' }],
-  ['beta-1009-g', { displayName: 'Agent Beta-1009-G' }],
+  ['beta-1009-e', { displayName: 'Agent Figaro 1012' }],
+  ['beta-1009-f', { displayName: 'Agent primo jose luis ;)' }],
+  ['beta-1009-g', { displayName: 'Inspector Juan Nogales' }],
   ['beta-1009-h', { displayName: 'Agent Beta-1009-H' }],
 ]);
 
